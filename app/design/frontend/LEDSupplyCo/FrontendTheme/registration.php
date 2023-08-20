c4324131c471138595b0c7b83207acb72999cbf4
Christopher Gilbert <cgilbert@ledsupplyco.com>
2023-08-20 16:32:27 -0400

    Homepage edits

---
+++ app/design/frontend/LEDSupplyCo/FrontendTheme/registration.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * BrandCrock Industryarc Theme in Magento2
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GNU General Public License
+ *
+ * DISCLAIMER
+ *
+ * Do not add or edit the files if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category   BrandCrock
+ * @package    BrandCrock_Industryarc
+ * @copyright  Copyright (c) BrandCrock (https://www.brandcrock.com/)
+ * @license    GNU General Public License
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/LEDSupplyCo/FrontendTheme',
+    __DIR__
+);
